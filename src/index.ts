e46b45f86acc76cc18a12f8bad8b7b6cb7370e27
mrfrac <fracturer@gmail.com>
2018-08-03 13:34:32 +0500

    small fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+import * as formulas from './formulas';
 import { AlterationEnum, Note } from './Note';
 import { Scale } from './Scale';
 
 export {
+  formulas,
   AlterationEnum,
   Note,
   Scale,
